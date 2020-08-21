db7480088fef9eec77a77ee739e0092fdc012b4d
Adhitya Yudha <adhityayudha36@gmail.com>
2020-08-21 16:51:21 +0700

    up2

--- app/src/main/java/com/example/escort/RegActivity.java
+++ app/src/main/java/com/example/escort/RegActivity.java
@@ -3,7 +3,6 @@ package com.example.escort;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.Layout;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.util.Patterns;
